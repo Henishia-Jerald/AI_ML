46b96844c654927b3a86c2e9f76b3de72170ec4f
Henishia Jerald <henishiaj@gmail.com>
2025-01-18 10:38:08 -0800

    Modified reuirements and setup files

--- AI_ML_Python_Projects/Cyber_Security/setup.py
+++ AI_ML_Python_Projects/Cyber_Security/setup.py
@@ -1,26 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages 
 from typing import List
 
+HYPHEN_E_DOT = "-e ."
 
-HYPEN_E_DOT='-e .'
-
-def get_requirements(file_path:str) -> List[str]:
-    # This function will return list of requirements
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n"," ") for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-        return requirements
-
+def get_requitements(file_path:str) -> list[str]:
+    with open(file_path,'r') as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.strip() for req in requirements if req.strip() and req.strip()!= HYPHEN_E_DOT]
+    return requirements
 
 setup(
-name = 'CyberSecurity',
-version='0.0.1',
-author='henishia',
-author_email='henishiaj@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-)
+    name= "CyberSecurity",
+    version= "0.0.1",
+    author= "Arockia Henishia",
+    author_email="henishiaj@gmail.com",
+    install_requires = get_requitements('requirements.txt')
 
+)
