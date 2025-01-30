abae394b79b0710e03c1d84919872fc0d55fc9e3
Henishia Jerald <henishiaj@gmail.com>
2025-01-30 10:24:17 -0800

    Logging and Exception

---
+++ AI_ML_Python_Projects/Cyber_Security/src/exception.py
@@ -0,0 +1,19 @@
+import sys
+from logger import logging
+
+def error_message_details(error, error_detail:sys):
+    _,_, tb_exc =error_detail.exc_info()
+    file_name = tb_exc.tb_frame.f_code.co_filename
+    line_no = tb_exc.tb_lineno
+    error_message = "Error has occured in the script name [{0}], line number [{1}] and error message [{2}]".format(file_name,line_no,error)
+
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_message = error_message_details(error_message,error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
